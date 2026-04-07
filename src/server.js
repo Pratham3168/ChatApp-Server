50c7807bfc318041e4895ef62c32baac298cc6b9
Pratham3168 <prathamtanwar593@gmail.com>
2026-04-07 23:36:03 +0530

    check

--- src/server.js
+++ src/server.js
@@ -36,8 +36,8 @@ app.use(express.json({ limit: REQUEST_BODY_LIMIT }));
 app.use(express.urlencoded({ extended: true, limit: REQUEST_BODY_LIMIT }));
 app.use(cookieParser());
 
-app.get('/api/health', (_, res) => {
-  res.status(200).json({ ok: true, service: 'chat-app-server' });
+app.get('/', (req, res) => {
+  res.send("Server is running");
 });
 
 app.use('/api/auth',authRoutes);
